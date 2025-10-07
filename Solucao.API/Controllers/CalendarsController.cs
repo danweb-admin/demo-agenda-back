e52d2bc46c50f0b58fa5653aec941d002d5cc78c
danweb-admin <143641802+danweb-admin@users.noreply.github.com>
2025-10-06 23:16:20 -0300

    Update CalendarsController.cs

--- Solucao.API/Controllers/CalendarsController.cs
+++ Solucao.API/Controllers/CalendarsController.cs
@@ -257,7 +257,7 @@ namespace Solucao.API.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> CalendarView([FromQuery] DateTime startDate, DateTime endDate)
         {
-            Console.WriteLine("calendar/view");
+            Console.WriteLine($"calendar/view -> {DateTime.Now} -> Motorista");
             
             return Ok(await calendarService.CalendarView(startDate, endDate, true));
         }
